9deaa79fbb4b60d0b9fbab3829a2c870173a7351
kaigood <13698630774@163.com>
2016-03-29 14:40:07 +0800

    添加文件

---
+++ ZhongHong.Core/ContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+
+namespace ZhongHong.Core
+{
+    /// <summary>
+    /// 数据上下文工厂
+    /// </summary>
+    public class ContextFactory
+    {
+        /// <summary>
+        /// 获取当前线程的数据上下文
+        /// </summary>
+        /// <returns>数据上下文</returns>
+        public static ZhongHongyContext CurrentContext()
+        {
+            ZhongHongyContext _nContext = CallContext.GetData("NineskyContext") as ZhongHongyContext;
+            if (_nContext == null)
+            {
+                _nContext = new ZhongHongyContext();
+                CallContext.SetData("NineskyContext", _nContext);
+            }
+            return _nContext;
+        }
+    }
+}
---
+++ ZhongHong.Core/ZhongHongyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ZhongHong.Core
+{
+    public class ZhongHongyContext:DbContext
+    {
+        public ZhongHongyContext(): base("DefaultConnection")
+        {
+            Database.SetInitializer<ZhongHongyContext>(new CreateDatabaseIfNotExists<ZhongHongyContext>());
+        }
+    }
+}
