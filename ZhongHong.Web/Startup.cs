4e6ec4bb0c2b77a152492a3ae49327587aed8859
kaigood <13698630774@163.com>
2016-03-29 11:52:55 +0800

    web

---
+++ ZhongHong.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZhongHong.Web.Startup))]
+namespace ZhongHong.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
